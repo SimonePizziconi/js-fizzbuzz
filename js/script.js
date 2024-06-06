57ee14d29bdc9facbf7d043cc6c884e108413cdb
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-06 23:27:54 +0200

    added square.innerHTML and remove append

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ for (let i = 1; i <= 100; i++){
 const container = document.getElementById("container");
 
 // Crea un elemento da mettere dentro al contenitore
-let square = document.createElement ("div");
+const square = document.createElement ("div");
 container.append(square);
 
 // Aggiungi una classe all'elemento square creato
@@ -13,28 +13,22 @@ square.classList.add("box");
 console.log(square, container);
     
 if (i % 3 === 0 && i % 5 === 0){ //  e che per i multipli di 3 e 5 stampi “FizzBuzz” al posto del numero
-        const textFizzBuzz = "FizzBuzz";
         // aggiungi un elemento html con il numero o la stringa corretta da mostrare.
-        square.append(textFizzBuzz);
+        square.innerHTML += "FizzBuzz"
         // aggiungi style per differenziare gli "square"
         square.style.backgroundColor = "red";
-        console.log(textFizzBuzz);
 
     } else if (i % 3 === 0){ //  e per i multipli di 3 stampi “Fizz”.
-        const textFizz = "Fizz";
         // aggiungi un elemento html con il numero o la stringa corretta da mostrare.
-        square.append(textFizz);
+        square.innerHTML += "Fizz"
         // aggiungi style per differenziare gli "square"
         square.style.backgroundColor = "green";
-        console.log(textFizz);
 
     } else if (i % 5 === 0){// e per i numeri che sono multipli di 5 stampi “Buzz”.
-        const textBuzz = "Buzz";
         // aggiungi un elemento html con il numero o la stringa corretta da mostrare.
-        square.append(textBuzz);
+        square.innerHTML += "Buzz"
         // aggiungi style per differenziare gli "square"
         square.style.backgroundColor = "yellow";
-        console.log(textBuzz);
         
     } else {
         // aggiungi un elemento html con il numero o la stringa corretta da mostrare.
